116bc081ee8be19b84a1e7de9ffe1959357f5a1a
adwait-01 <222039792+adwait-01@users.noreply.github.com>
2026-06-08 07:30:49 +0530

    props before using Swiggy API

--- App.js
+++ App.js
@@ -28,7 +28,10 @@ const styleCard = {
   backgroundColor: "#f0f0f0",
 };
 
-const RestaurantCard = () => {
+const RestaurantCard = (props) => {
+  // console.log(props);
+  // 1. Instead of (props), we can write ({resName, cuisine}) -> This is known as 'object destructuring' which we do in javascript. Instead of writing {props.resName} in the above case, we directly write {resName} in this case when we use that prop.
+  // 2. We can also do -> const {resName, cuisine} = props; here and then use the same way instead of doing directly inside () -> which(1) is called object destructuring on the fly.
   return (
     <div className="res-card" style={styleCard}>
       <img
@@ -36,25 +39,27 @@ const RestaurantCard = () => {
         alt="res-logo"
         src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSk78m6tFENy9jRoCoL9y1vOiWL5WTRw3yUpg&s"
       />
-      <h3>Shawarma Express</h3>
-      <p>Middle Eastern, Lebanese, Mediterranean</p>
+      <h3>{props.resName}</h3>
+      <p>{props.cuisine}</p>
       <p>4.3 stars</p>
       <p>29 mins</p>
     </div>
   );
 };
 
+// Generally data is not passed as string directly to each component right. 
+// A data comes from backend to us in the form of a JSON. 
+
 const Body = () => {
   return (
     <div className="body">
       <div className="search">Search</div>
       <div className="res-container">
-        <RestaurantCard />
-        <RestaurantCard />
-        <RestaurantCard />
-        <RestaurantCard />
-        <RestaurantCard />
-        <RestaurantCard />
+        <RestaurantCard
+          resName="Shawarma Express"
+          cuisine="Middle Eastern, Lebanese, Mediterranean"
+        />
+        <RestaurantCard resName="KFC" cuisine="Burger, Fast Food" />
       </div>
     </div>
   );
