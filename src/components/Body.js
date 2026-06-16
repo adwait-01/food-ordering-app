76db3ab23cce2bfec57c4a0aaba11f3a1aa06b88
adwait-01 <222039792+adwait-01@users.noreply.github.com>
2026-06-16 07:03:15 +0530

    Remove mockData.js & add data from live Swiggy's API into listOfRestaurants

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,9 +1,29 @@
 import RestaurantCard from "./RestaurantCard";
-import { resList } from "../utils/mockData";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 const Body = () => {
-  const [listOfRestaurants, setListOfRestaurants] = useState(resList);
+  const [listOfRestaurants, setListOfRestaurants] = useState([]);
+
+  useEffect(() => {
+    console.log("useEffect called");
+    fetchData();
+  }, []);
+
+  // console.log("Body rendered")
+
+  const fetchData = async () => {
+    const data = await fetch(
+      "https://www.swiggy.com/dapi/restaurants/list/v5?lat=20.26570291996979&lng=85.84314675817616&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING"
+    );
+    const json = await data.json();
+    // console.log(json);
+    // Optional chaining:
+    setListOfRestaurants(
+      json?.data?.cards[3]?.card?.card?.gridElements?.infoWithStyle
+        ?.restaurants || []
+    );
+  };
+
   return (
     <div className="body">
       <div className="filter">
