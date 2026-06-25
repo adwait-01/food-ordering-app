0a057ed0455659c597412499b58c235f2fce108c
adwait-01 <222039792+adwait-01@users.noreply.github.com>
2026-06-25 10:54:51 +0530

    Add CORS proxy

--- src/components/Body.js
+++ src/components/Body.js
@@ -13,7 +13,7 @@ const Body = () => {
 
   const fetchData = async () => {
     const data = await fetch(
-      "https://www.swiggy.com/dapi/restaurants/list/v5?lat=22.2421273&lng=84.8075572&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING"
+      "https://corsproxy.io/?url=https://www.swiggy.com/dapi/restaurants/list/v5?lat=22.2421273&lng=84.8075572&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING"
     );
     const json = await data.json();
     const restaurantCard = json?.data?.cards?.find(
