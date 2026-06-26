8361c2b555e0382e159f91629b44aa8792c29404
adwait-01 <222039792+adwait-01@users.noreply.github.com>
2026-06-26 12:32:22 +0530

    Add About & Contact Page using Routing

--- src/App.js
+++ src/App.js
@@ -2,6 +2,9 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import Header from "./components/Header"; // We could also write "./components/Header.js"
 import Body from "./components/Body";
+import About from "./components/About";
+import Contact from "./components/Contact";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
@@ -14,4 +17,19 @@ const AppLayout = () => {
   );
 };
 
-root.render(<AppLayout />);
+const appRouter = createBrowserRouter([
+  {
+    path: "/",
+    element: <AppLayout />,
+  },
+  {
+    path: "/about",
+    element: <About />,
+  },
+  {
+    path: "/contact",
+    element: <Contact />,
+  },
+]);
+
+root.render(<RouterProvider router={appRouter} />);
