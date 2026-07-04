af9a4fcf0c4c6958e15096deed3aa6d6efbc406e
adwait-01 <222039792+adwait-01@users.noreply.github.com>
2026-07-04 14:55:11 +0530

    Create multiple routes using react-router-dom library

---
+++ src/components/Error.js
@@ -0,0 +1,18 @@
+import { useRouteError } from "react-router-dom";
+
+const Error = () => {
+  const err = useRouteError();
+  const { status, statusText } = err;
+  console.log(err);
+  return (
+    <div>
+      <h1>Ooops!!!</h1>
+      <h1>Something Went Wrong...</h1>
+      <h3>
+        {status}: {statusText}{" "}
+      </h3>
+    </div>
+  );
+};
+
+export default Error;
